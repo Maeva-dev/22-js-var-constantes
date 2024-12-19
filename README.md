b3c900a291e6f62868755744664a57e2bbf5e7ae
Maeva-dev <maevaac.devel@gmail.com>
2024-12-19 10:10:45 -0300

    constantes e variaveis

--- README.md
+++ README.md
@@ -1,1 +1,19 @@
 # 22-js-var-constantes
+#tipos de dados
+.string=texto
+.nunber=numeros
+.boolean=verd./falso, logico
+
+.array
+.object
+.null
+.underfined
+.simbol
+
+## Erros comuns
+
+- ReferenceErro: toda vez que você escrever algo que o javascript não encontrar.
+
+- SyntaxError: toda vez que voce escrever codigo errado.
+
+- TypeError: toda vez que voce errar algo relacionado ao tipo
---
+++ index.js
@@ -0,0 +1,19 @@
+document.write(`<h1 class="titulo">Olá mundo</h1>`);
+
+document.write(`<h1 class="titulo">Quanto é 2+2?</h1>`);
+let resultado = 2+2;
+document.write(`<h1 class="titulo">${resultado}</h1>`);
+document.write(`<h1 class="titulo">Adicione o dobro</h1>`);
+document.write(`<h1 class="titulo">${resultado + 8}</h1>`);
+
+let nome = Leo;
+
+document.write(`<h1>Seja bem-VideoEncoder(a) ${nome}</h1>`);
+
+let habilitado = true;  //true ||false
+
+document.write(`<h1>0 ${nome} ${habilitado ? `é`:'não é'} habilitado</h1>`);
+
+const nome_Duplo = "camelCase";
+const nome_Duplo = "camelCase";
+const nome_Duplo = "camelCase";
